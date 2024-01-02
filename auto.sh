d2c5ec161a72c19bc115975559109a4b70a3583a
Zhu Langpi <799715349@qq.com>
2024-01-02 19:25:42 +0800

    ENH: improve vim config
    - vim config
    - delete vim.tar.gz and copy directly
    
    Signed-off-by: Zhu Langpi <799715349@qq.com>

--- auto.sh
+++ auto.sh
@@ -1,10 +1,8 @@
 rm -rf ~/.vim*
 touch ~/.vimrc
 rm -rf ~/.vim/auto.sh
-cp vim.tar.gz ~/
-cd ~/
-tar -xvf vim.tar.gz
-rm ~/vim.tar.gz
+mkdir ~/.vim
+cp -rf * ~/.vim
 echo "source ~/.vim/vimrcs/main" >> ~/.vimrc
 echo "source ~/.vim/vimrcs/bundle" >> ~/.vimrc
 sudo apt-get install ctags
